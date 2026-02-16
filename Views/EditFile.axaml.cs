7bed45aecf5b57e6aeb55d82dd09c470ba1fbed7
Michael Babienco <mbabienco@southern.edu>
2026-02-16 14:29:32 +0900

    Add Edit page

---
+++ Views/EditFile.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReceiptPDFBuilder.Views
+{
+    public partial class EditFile : UserControl
+    {
+        public EditFile()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
