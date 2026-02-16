37b927d601a1c6aa335fd97dc523d16aaf6dcc23
Michael Babienco <mbabienco@southern.edu>
2026-02-16 13:06:52 +0900

    Add view for deleting item

---
+++ Views/WarningDeleteItem.axaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ReceiptPDFBuilder.Views
+{
+    public partial class WarningDeleteItem : UserControl
+    {
+        public WarningDeleteItem()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
