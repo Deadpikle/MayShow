32228e143fbb93af098bb4ab10a9ceb702387cab
Michael Babienco <mbabienco@southern.edu>
2026-03-03 11:10:56 +0900

    Show licenses in About view
    Closes #4

--- src/Views/AboutView.axaml.cs
+++ src/Views/AboutView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -10,6 +11,27 @@ namespace MayShow.Views
         public AboutView()
         {
             this.InitializeComponent();
+
+            // set license text
+            var licenseFileName = "Assets/LICENSES.txt";
+            var licenseText = "";
+            if (File.Exists(licenseFileName))
+            {
+                licenseText = File.ReadAllText(licenseFileName);
+            }
+            else
+            {
+                licenseFileName = "../Resources/Assets/LICENSES.txt";
+                if (File.Exists(licenseFileName))
+                {
+                    licenseText = File.ReadAllText(licenseFileName);
+                }
+                else
+                {
+                    licenseText = "Error: Unable to find license file!";
+                }
+            }
+            LicenseTextBlock.Text = licenseText;
         }
     }
 }
