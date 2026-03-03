1efd9993bf3b0dfacaaaccf3f00cd8a9e1fe6f9d
Michael Babienco <mbabienco@southern.edu>
2026-03-03 11:52:34 +0900

    Clean before release in linux build

--- installers/build-linux.sh
+++ installers/build-linux.sh
@@ -11,12 +11,15 @@ if [ ! -d "$SRC_DIR" ]; then
 fi
 cd "$SRC_DIR"
 echo "Building release for linux-x64..."
+dotnet clean -c Release
 dotnet publish -c Release -r linux-x64 -p:StripSymbols=False -p:PublishAot=False
 echo "Zipping up linux-x64..."
 cd bin/Release/net10.0/linux-x64/publish
 zip -r "../../../../MayShow $VERSION linux-x64.zip" .
 cd ../../../../../
+# -----
 echo "Building release for linux-arm64..."
+dotnet clean -c Release
 dotnet publish -c Release -r linux-arm64 -p:StripSymbols=False -p:PublishAot=False
 cd bin/Release/net10.0/linux-arm64/publish
 zip -r "../../../../MayShow $VERSION linux-arm64.zip" .
